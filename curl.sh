c32f66494733323f911d0edbe86298dfea24143a
saucelabsautomationtesting <saucelabsautomationtesting@gmail.com>
2017-04-04 14:09:30 +0100

    Add files via upload

---
+++ curl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -o /tmp/saucelabs_file.csv https://raw.githubusercontent.com/ravikumarachi/menu/master/saucelabs_file.csv
+curl -o /tmp/saucelabs_file1.csv https://raw.githubusercontent.com/ravikumarachi/menu/master/saucelabs_file1.csv
+curl -o /tmp/batch_upload_gender_validation_testdata.csv https://raw.githubusercontent.com/ravikumarachi/menu/master/batch_upload_gender_validation_testdata.csv
+
+
