234484654ec1562afcb7fb64297cf4f670e604b6
saucelabsautomationtesting <saucelabsautomationtesting@gmail.com>
2017-04-04 14:19:41 +0100

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-curl -o /tmp/saucelabs_file.csv https://raw.githubusercontent.com/ravikumarachi/menu/master/saucelabs_file.csv
-curl -o /tmp/saucelabs_file1.csv https://raw.githubusercontent.com/ravikumarachi/menu/master/saucelabs_file1.csv
-curl -o /tmp/batch_upload_gender_validation_testdata.csv https://raw.githubusercontent.com/ravikumarachi/menu/master/batch_upload_gender_validation_testdata.csv
+curl -o /tmp/batch_upload_test.csv https://raw.githubusercontent.com/saucelabsautomationtesting/export/master/batch_upload_test.csv
 
 
