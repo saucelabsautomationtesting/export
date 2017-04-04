4c9e21e3e094434d4866ce96b0125de199310ef7
saucelabsautomationtesting <saucelabsautomationtesting@gmail.com>
2017-04-04 14:58:33 +0100

    Update curlWin.sh

--- curlWin.sh
+++ curlWin.sh
@@ -1,3 +1,3 @@
 @echo off
-bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/saucelabsautomationtesting/export/master/batch_upload_test.csv C:\batch_upload_test.csvto
+bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/saucelabsautomationtesting/export/master/batch_upload_test.csv C:\batch_upload_test.csv
 
